c423280da770517c0a2fcc46d73771db9f9cf98d
Jennifer <gnzlezjennifer@gmail.com>
2023-03-06 11:51:54 -0600

    css de las tarjetas

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const contenido = document.querySelector('#contenido')
 const cardTop = document.querySelector('#cardTop').content
 const fragment = document.createDocumentFragment()
+const btnBuscar = document.getElementById('buscador')
 let topTwoHundred = []
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -30,8 +31,23 @@ const creaCards = () => {
     topTwoHundred.forEach( (song) => {
         cardTop.querySelector('img').setAttribute("src", song.trackMetadata.displayImageUri)
         cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+        let artists = ''
+        let size = song.trackMetadata.artists.length
+        song.trackMetadata.artists.forEach((item, index) => {
+            if (index === size - 1){
+                artists += item.name
+            } else {
+                artists += item.name + ' / '
+            }
+        })
+        cardTop.querySelector('.artistname').textContent = artists
         const clone = cardTop.cloneNode(true)
         fragment.appendChild(clone)
     })
     contenido.appendChild(fragment)
 }
+
+btnBuscar.addEventListener('keypress', () => {
+    console.log('tecla', btnBuscar.value)
+    
+})
